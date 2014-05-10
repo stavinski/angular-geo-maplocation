cee90a0e1da7d74bcb0ec3379bd0f513ac68c96a
Michael Cromwell <stavinski_2000@yahoo.com>
2014-05-10 10:26:03 +0100

    adding dist into release

---
+++ dist/angular-geo-maplocation.js
@@ -0,0 +1,41 @@
+'use strict';
+angular.module('angular-geo-maplocation', ['google-maps']).directive('geoMaplocation', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      coords: '=',
+      zoom: '=?',
+      tooltip: '=?'
+    },
+    template: '<google-map center="map.start" zoom="map.zoom" draggable="true" options="map.options"><marker coords="coords" options="marker.options" events="marker.events"></marker></google-map>',
+    controller: [
+      '$scope',
+      function ($scope) {
+        if (!$scope.coords) {
+          throw new Error('coords must be supplied');
+        }
+        $scope.map = {
+          zoom: $scope.zoom || 8,
+          options: { streetViewControl: false }
+        };
+        $scope.marker = {
+          options: {
+            draggable: true,
+            title: $scope.tooltip || 'Drag marker to geo-location'
+          },
+          events: {
+            dragend: function (marker) {
+              $scope.$apply(function () {
+                $scope.coords.latitude = marker.getPosition().lat();
+                $scope.coords.longitude = marker.getPosition().lng();
+              });
+            }
+          }
+        };
+        $scope.$watch('coords', function () {
+          $scope.map.start = $scope.coords;
+        });
+      }
+    ]
+  };
+});
